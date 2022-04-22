74f368f78dac0853895ccda4a8a747df2e96f3fc
buzink <rjf.buzink@gmail.com>
2022-04-22 17:28:38 +0200

    Added crontab command in comments

--- autoupgrade.sh
+++ autoupgrade.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-  if git remote -v update 2>&1 | grep "master" | grep "up to date" &> /dev/null
-  then
-    echo "Repository unchanged"
-  else
-    echo "Repository changed, updating"
-    ./pipeline.sh
-  fi
+##chmod +x autoupgrade.sh && { crontab -l; echo "*/30 * * * * ~/BluOS-from-across-the-room-display/autoupgrade.sh"; } | crontab -
+if git remote -v update 2>&1 | grep "master" | grep "up to date" &> /dev/null
+then
+  echo "Repository unchanged"
+else
+  echo "Repository changed, updating"
+  ./pipeline.sh
+fi
